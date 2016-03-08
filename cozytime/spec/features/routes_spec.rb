80b279d65b61c2c156ae02fe2b66d20f3b01dbb6
Lauren Reid <lauren.g.reid@gmail.com>
2016-03-08 11:12:03 -0600

    Add genre show page and links on movie show page and started index tests

--- cozytime/spec/features/routes_spec.rb
+++ cozytime/spec/features/routes_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe "homepage" do
   it "has correct url" do
     visit '/'
-    expect(current_path).to eq('/movies/index')
+    expect(current_path).to eq('/')
   end
 end
